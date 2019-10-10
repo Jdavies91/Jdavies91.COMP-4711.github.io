4b6e1da7ff87fbd9908630e1a6572511c6b8690a
Jdavies91 <Jdavies86@my.bcit.ca>
2019-10-09 21:03:35 -0700

    Add to Lab5

---
+++ Lab5/Lab5Nodejs.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const hostname = 'comp4711.a01011881.online';
+const port = 3000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
